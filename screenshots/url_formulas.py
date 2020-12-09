c43928faf2d0d0dd54035d3f67a4ab91b29c383a
theomichel <theomichel@hotmail.com>
2020-12-09 15:01:18 -0800

    some test code for building urls
    Not intending to merge this, just thought it would be a good way to share the code. These are all the URLs that have formulas in the states tab of the dev sheet.

---
+++ screenshots/url_formulas.py
@@ -0,0 +1,26 @@
+from datetime import date 
+from datetime import timedelta
+
+# NOTE: the %#d (day of month not zero padded) and %#m (number of month not zero padded) formatting directives 
+#       are different depending on whether you're running on unix or windows. Unix is %-d, windows is %#d. 
+# NOTE: %B (full month name) makes capitalized months, which doesn't match the existing urls. However, these web servers are case-insensitive. (so far)
+
+# =CONCATENATE("https://www.mass.gov/doc/covid-19-dashboard-",LOWER(TEXT(now(),"mmmm")),"-",LOWER(TEXT(now(),"d")),"-" , YEAR(NOW()),"/download")
+print("https://www.mass.gov/doc/covid-19-dashboard-december-9-2020/download")
+print(eval("date.today().strftime(\"https://www.mass.gov/doc/covid-19-dashboard-%B-%#d-%Y/download\").lower()"))
+
+# =CONCATENATE("https://www.mass.gov/doc/weekly-covid-19-public-health-report-",LOWER(TEXT(now(),"mmmm")),"-",LOWER(TEXT(now(),"d")),"-" , YEAR(NOW()),"/download")
+print("https://www.mass.gov/doc/weekly-covid-19-public-health-report-december-9-2020/download")
+print(eval("date.today().strftime(\"https://www.mass.gov/doc/weekly-covid-19-public-health-report-%B-%#d-%Y/download\").lower()"))
+
+# =CONCATENATE("https://dphhs.mt.gov/Portals/85/publichealth/documents/CDEpi/DiseasesAtoZ/2019-nCoV/Status%20Update%20Data%20Report_",MONTH(NOW()),if(DAY(NOW())<10,CONCATENATE(0,DAY(NOW())),DAY(NOW())),"20",TEXT(NOW(),"y"),".pdf")
+print("https://dphhs.mt.gov/Portals/85/publichealth/documents/CDEpi/DiseasesAtoZ/2019-nCoV/Status%20Update%20Data%20Report_12092020.pdf")
+print(eval("date.today().strftime(\"https://dphhs.mt.gov/Portals/85/publichealth/documents/CDEpi/DiseasesAtoZ/2019-nCoV/Status%%20Update%%20Data%%20Report_%m%d%Y.pdf\")"))
+
+# =CONCATENATE("https://coronavirus.health.ok.gov/sites/g/files/gmc786/f/eo_-_covid-19_report_-_",MONTH(NOW()-1),"-",DAY(NOW()-1),"-",TEXT(NOW()-1,"y"),".pdf")
+print("https://coronavirus.health.ok.gov/sites/g/files/gmc786/f/eo_-_covid-19_report_-_12-8-20.pdf")
+print(eval("(date.today() - timedelta(days=1)).strftime(\"https://coronavirus.health.ok.gov/sites/g/files/gmc786/f/eo_-_covid-19_report_-_%#m-%#d-%y.pdf\")"))
+
+# =CONCATENATE("https://coronavirus.health.ok.gov/sites/g/files/gmc786/f/","2020",".",MONTH(NOW()-1),".",DAY(NOW()-1),"_weekly_epi_report.pdf")
+print("https://coronavirus.health.ok.gov/sites/g/files/gmc786/f/2020.12.8_weekly_epi_report.pdf")
+print(eval("(date.today() - timedelta(days=1)).strftime(\"https://coronavirus.health.ok.gov/sites/g/files/gmc786/f/%Y.%#m.%#d_weekly_epi_report.pdf\")"))
