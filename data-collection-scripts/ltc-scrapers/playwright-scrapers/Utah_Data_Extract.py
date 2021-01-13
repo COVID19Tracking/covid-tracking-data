920fe89498b0d17dab09dcede02d5ada0477796d
Carl Chen <carllcchen@Carls-MacBook-Pro.local>
2021-01-12 23:47:25 -0500

    UpdatedVersion

--- data-collection-scripts/ltc-scrapers/playwright-scrapers/Utah_Data_Extract.py
+++ data-collection-scripts/ltc-scrapers/playwright-scrapers/Utah_Data_Extract.py
@@ -2,46 +2,68 @@ import json
 from datetime import datetime
 from pytz import timezone
 from urllib.request import urlopen
+import sys
+import csv
+from io import StringIO
 
 def load_json():
     url = ("https://services6.arcgis.com/KaHXE9OkiB9e63uE/ArcGIS/rest/services/COVID19_Long_Term_Care_Facility_Impacts/FeatureServer/273/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=true&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=")
     response = urlopen(url)
-    if response.getcode() == 200:
+    status_code = response.getcode()
+    if status_code == 200:
         source = response.read()
         dict_data = json.loads(source)
     else:
-        print("Error")
-    #print(response.read())
-    #data = json.loads(response.read())
-
-    #f = open("Facilities_with_Active_Outbreaks.json", "r")
-    #data = f.read()
-    CSV_file = open("CSV_Data_Extract.csv", "w")
+        print("URL request failed with status code: " + status_code)
+        sys.exit(1)
 
     if not dict_data is None:
         features_list = dict_data["features"]
-        for item in features_list:
-            fmt = datetime.today().strftime('%Y%m%d')
-            now_time = datetime.now(timezone('US/Eastern'))
-            line_data = now_time.strftime(fmt)
-            line_data += "," + "UT"
-            line_data += "," + "" + "," + ""
-            line_data += "," + item["attributes"]["Facility_Name"]
-            line_data += "," + item["attributes"]["Facility_Type"]
-            line_data += "," + "" + "," + "" + "," + "" + "," + "" + "," + "" + "," + ""
-            line_data += "," + item["attributes"]["Resolved_Y_N"]
-            line_data += "," + ""
-            if item["attributes"]["Postive_Patients_Desc"] == "No Resident Cases":
-                line_data += "," + "0"
-            else:
-                line_data += "," + item["attributes"]["Postive_Patients_Desc"]
-            line_data += "," + "" + "," + "" + "," + "" + "," + "" + "," + "" + "," + "" + "," + ""
-            if item["attributes"]["Resolved_Y_N"] == "N":
-                line_data += "," + item["attributes"]["Postive_Patients_Desc"]
-            else:
-                line_data += "," + ""
-            line_data += "," + "" + "," + "" + "," + "" + "," + "" + "," + "" + "," + "" + "," + ""
-            line_data += "," + item["attributes"]["Dashboard_Display"]
-            CSV_file.write(line_data + "\n")
+        si = StringIO()
+        fieldnames=["Date Collected", "State", "blank1",
+                    "blank2", "Facility_Name", "Facility_Type", "blank3",
+                    "blank4", "blank5", "blank6", "blank7", "blank8", "Resolved_Y_N",
+                    "blank9", "Postive_Patients_Desc", "blank10", "blank11", "blank12",
+                    "blank13", "blank14", "blank15", "blank16",
+                    "Unresolved_Postive_Patients_Desc",
+                    "blank17", "blank18", "blank19", "blank20",  "blank21", "blank22", "blank23",
+                    "Dashboard_Display"]
+        writer = csv.DictWriter(si, fieldnames)
 
-        CSV_file.close()
+        writer.writeheader()
+
+        for item in features_list:
+            row_data = '{"Date Collected": '
+            row_data += datetime.now(timezone('US/Eastern')).strftime('%Y%m%d')
+            row_data += ', "State": "UT", "blank1": "", "blank2": "", "Facility_Name": "'
+            row_data += (item["attributes"]["Facility_Name"])
+
+            row_data += '","Facility_Type": "'
+            row_data += item["attributes"]["Facility_Type"]
+
+            row_data += '", "blank3":"", "blank4": "", "blank5": "", "blank6": "", "blank7":"", "blank8": ""'
+            row_data += ', "Resolved_Y_N": "'
+            row_data += item["attributes"]["Resolved_Y_N"]
+
+            row_data += '", "blank9": "", "Postive_Patients_Desc": "'
+            if item["attributes"]["Postive_Patients_Desc"] == "No Resident Cases":
+                row_data += '0'
+            else:
+                row_data += item["attributes"]["Postive_Patients_Desc"]
+
+            row_data += '", "blank10":"","blank11":"","blank12":"","blank13":"","blank14":"","blank15":"","blank16": ""'
+            row_data += ', "Unresolved_Postive_Patients_Desc": "'
+            if item["attributes"]["Resolved_Y_N"] == "N":
+                row_data += item["attributes"]["Postive_Patients_Desc"]
+            else:
+                row_data += ''
+            row_data += '", "blank17":"","blank18":"","blank19": "", "blank20":"", "blank21":"", "blank22":"","blank23":""'
+            row_data += ', "Dashboard_Display": "'
+            row_data += item["attributes"]["Dashboard_Display"]
+
+            row_data += '"}'
+
+            row_dict = json.loads(row_data)
+            writer.writerow(row_dict)
+
+        print(si.getvalue())
